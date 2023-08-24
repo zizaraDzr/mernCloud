9192ec237e030fb6f5494cd520ae2de1bc2c0099
zizaraDzr <elikdzr@mail.ru>
2023-08-24 16:42:26 +0300

    init server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  return <div>145523</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        fontSize: "25px",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div>0</div>
+    </div>
+  );
 }
 
 export default App;
