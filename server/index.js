f6d7e62bd5a94c480a2618967cac472b7788afe1
zizaraDzr <elikdzr@mail.ru>
2023-08-25 16:28:03 +0300

    подключение к монго

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+
+
+const app = express()
+const PORT = config.get('serverPort')
+const DBURL = config.get('dbURL')
+console.log(DBURL)
+
+const start = async () => {
+    try {
+        await mongoose.connect(DBURL)
+        app.listen(PORT, () => {
+            console.log(`Сервер запущен ${PORT}--- бд подлючено ${DBURL}`)
+        })
+    } catch (error) {
+        console.log('Ошибка', error)
+    }
+}
+
+start()
