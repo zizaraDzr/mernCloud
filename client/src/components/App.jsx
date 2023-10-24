2156fa5b933611df890004531ef5672d8fbcf610
zizaraDzr <elikdzr@mail.ru>
2023-10-24 17:07:13 +0300

    8. форма авторизации

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
+import React from "react";
 import Navbar from "./navbar/Navbar";
-import './app.css'
-import {BrowserRouter, Route, Switch} from "react-router-dom";
-import Registration from "./registration/Registration";
+import "./app.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Registration from "./authorization/Registration";
+import Login from "./authorization/Login";
 
 function App() {
   return (
-      <BrowserRouter>
-          <div className='app'>
-              <Navbar/>
-              <div className="wrap">
-                  <Switch>
-                      <Route path="/registration" component={Registration}/>
-                  </Switch>
-              </div>
-          </div>
-      </BrowserRouter>
+    <BrowserRouter>
+      <div className="app">
+        <Navbar />
+        <div className="wrap">
+          <Switch>
+            <Route path="/registration" component={Registration} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -8,23 +8,23 @@ const defaultState = {
 
 export default function userReducer(state = defaultState, action) {
   switch (action.type) {
-    // case SET_USER:
-    //   return {
-    //     ...state,
-    //     currentUser: action.payload,
-    //     isAuth: true,
-    //   };
-    // case LOGOUT:
-    //   localStorage.removeItem("token");
-    //   return {
-    //     ...state,
-    //     currentUser: {},
-    //     isAuth: false,
-    //   };
+    case SET_USER:
+      return {
+        ...state,
+        currentUser: action.payload,
+        isAuth: true,
+      };
+    case LOGOUT:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        currentUser: {},
+        isAuth: false,
+      };
     default:
       return state;
   }
 }
 
-// export const setUser = (user) => ({ type: SET_USER, payload: user });
-// export const logout = () => ({ type: LOGOUT });
+export const setUser = (user) => ({ type: SET_USER, payload: user });
+export const logout = () => ({ type: LOGOUT });
