41345b1f37705286c951fb4b6f08ab2a9e052f74
zizaraDzr <elikdzr@mail.ru>
2023-10-25 10:47:16 +0300

    8. форма авторизации

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./navbar/Navbar";
 import "./app.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Registration from "./authorization/Registration";
 import Login from "./authorization/Login";
+import { useDispatch, useSelector } from "react-redux";
+import { auth } from "../actions/user";
 
 function App() {
+  const isAuth = useSelector((state) => state.user.isAuth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(auth());
+  }, []);
+
   return (
     <BrowserRouter>
       <div className="app">
         <Navbar />
         <div className="wrap">
-          <Switch>
-            <Route path="/registration" component={Registration} />
-            <Route path="/login" component={Login} />
-          </Switch>
+          {!isAuth && (
+            <Switch>
+              <Route path="/registration" component={Registration} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          )}
         </div>
       </div>
     </BrowserRouter>
